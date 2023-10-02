a964b491a5f7873ad24a700ceaea0502c4c7c31f
Sandesh Nandagawe <sandagawe@gmail.com>
2023-10-02 15:20:04 +0530

    with employee details, media directory

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Employees(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='images')
+    designation = models.CharField(max_length=100)
+    email_address = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=12, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True) #when the records is first created
+    updated_at = models.DateTimeField(auto_now=True) # to records when any changes made to this field   
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name + ' / ' + self.designation
--- mysite/settings.py
+++ mysite/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'employees',
 ]
 
 MIDDLEWARE = [
@@ -113,3 +114,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#media files configuration
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
